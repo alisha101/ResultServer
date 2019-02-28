8c8b2a464a3d839255be5d643d7ba4ff58381fda
alisha101 <alafshafazlani@gmail.com>
2019-02-28 20:58:52 +0530

    yml file

--- src/main/java/com/dbconnection/DBConnection.java
+++ src/main/java/com/dbconnection/DBConnection.java
@@ -8,8 +8,8 @@ public class DBConnection {
 	public Connection connection() {
 		try{  
 			Class.forName("com.mysql.jdbc.Driver").newInstance();  
-//			con=DriverManager.getConnection( "jdbc:mysql://mydatabase.cqs57mhpwmsm.us-west-2.rds.amazonaws.com:3306/result","admin","admin123");
-			con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/result","root","root");
+			con=DriverManager.getConnection( "jdbc:mysql://mydatabase.cqs57mhpwmsm.us-west-2.rds.amazonaws.com:3306/result","admin","admin123");
+//			con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/result","root","root");
 			System.out.println(con);
 			return con;
 			
